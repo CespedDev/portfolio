3329b8034e8f63b9476e28b7db214cf9afa9703a
PC-002\Carlos <carloscf.correo@gmail.com>
2026-04-18 14:45:04 +0200

    Fix timeline

--- src/app/home-page/home-section-about/home-section-about.ts
+++ src/app/home-page/home-section-about/home-section-about.ts
@@ -3,10 +3,9 @@ import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home-section-about',
+  imports: [ButtonModule],
   templateUrl: './home-section-about.html',
   styleUrls: ['./home-section-about.scss'],
-  standalone: true,
-  imports: [ButtonModule]
 })
 export class HomeSectionAboutComponent {
 }
